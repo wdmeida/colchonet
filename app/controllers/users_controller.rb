6fffbb8cb5205765110199b54de1cacef09c198c
Wagner Almeida <wdmeida@gmail.com>
2017-01-18 11:01:35 -0200

    Interligação das funcionalidades prontas do sistema.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,20 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user,
+                notice: 'Cadastro atualizado com sucesso!'
+        else
+            render action: :edit
+        end
+    end
+
     private
 
     def user_params
