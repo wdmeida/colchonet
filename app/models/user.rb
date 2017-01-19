1161f22702e5bc6baed7eea103aa2f84d5df2556
Wagner Almeida <wdmeida@gmail.com>
2017-01-19 10:35:02 -0200

    Implementação da encriptação das senhas dos usuários.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
     #Verifica se os campos foram preenchidos.
     validates_presence_of :email, :full_name, :location, :password 
     
-    #Garante a confirmação da senha antes de ser salva.
-    validates_confirmation_of :password
-    
     #Verifica o tamanho do campo bio e bloqueia o mesmo de ser vazio.
     validates_length_of :bio, minimun: 30, allow_blank: false
    
@@ -16,4 +13,7 @@ class User < ApplicationRecord
     end
 
     validates_uniqueness_of :email
+
+    #Class macro responsável por fazer a encriptação da senha do usuário.
+    has_secure_password
 end
