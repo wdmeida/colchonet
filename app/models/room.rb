b9cb6fe8f72ea983532524dd2da80e4135f8504f
Wagner Almeida <wdmeida@gmail.com>
2017-01-23 15:37:50 -0200

    Implementação do helper para validação dos erros nos formulários de cadastro de quartos e usuários.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,10 @@
 class Room < ApplicationRecord
+    #Verifica a presença dos campos.
+    validates_presence_of :title, :location, :description
+    
+    #Valida a quantidade caracteres minimos inseridos no campo description.
+    validates_length_of :description, minimun: 20, allow_blank: false
+
     def complete_name
         "#{title}, #{location}"
     end
