3b41223fb1195f35f788e82687a63474b71cb6f2
Wagner Almeida <wdmeida@gmail.com>
2017-01-24 00:02:45 -0200

    Implementação da funcionalidade de seleção de idioma.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  #Define o filtro que será executado antes das ações, configurando o sistema de internacionalização.
+  before_action do
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  #Define a opção de idioma padrão para as rotas, caso o idioma não seja informado.
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :users
+  #Expressão regular para validar os idiomas suportados.
+  LOCALES = /en|pt\-BR/
+  
+  scope "(:locale)", locale: LOCALES do
+    resources :rooms
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/:locale' => 'home#index', locale: LOCALES
   root 'home#index'
 end
