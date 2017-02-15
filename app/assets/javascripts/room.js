9a919968d0083af93462a768ad0fcc71c33a8780
Wagner Almeida <wdmeida@gmail.com>
2017-02-15 10:22:38 -0200

    Costumização da tela de avaliação do usuário.

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -24,4 +24,31 @@ $(function() {
                         addClass(icon_class).
                         css('color', color));
     };
+
+    /* 
+       Adiciona e remove o destaque das estrelas. Baseado no elemento passado como parâmetro,
+       primeiro remove o destaque de todas as estrelas (CSS toggled) e em seguida adiciona-se
+       a mesma classe apenas ao elemento em destaque e os anteriores.
+    */ 
+    function highlightStars(elem) {
+        elem.parent().children('label').removeClass('toggled');
+        elem.addClass('toggled').prevAll('label').addClass('toggled');
+    }
+
+    highlightStars($('.review input:checked + label'));
+
+    var $stars = $('.review input:enabled ~ label');
+
+    $stars.on('mouseenter', function() {
+        highlightStars($(this));
+    });
+
+    $stars.on('mouseleave', function() {
+        highlightStars($('.review input:checked + label'));
+    });
+
+    $('.review input').on('change', function() {
+        $stars.off('mouseenter').off('mouseleave').off('click');
+        $(this).parent('form').submit();
+    });
 });
